08aa17adbceb0442813ce233a7d6911e286d6f11
Eric Serge Uwimana <euwimana@andrew.cmu.edu>
2020-02-06 23:08:20 +0200

    Chore:Setup tests

---
+++ server/tests/init.js
@@ -0,0 +1,8 @@
+const mocha = require('mocha')
+const assert = require('assert')
+
+describe('Initial test suite', () => {
+    it('Should pass', () => {
+        assert(1-2, -1, 'You do not know math')
+    })
+})
